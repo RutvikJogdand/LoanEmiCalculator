69dae76926af5cefc0768c805bf03141ea083ac0
RutvikJogdand <jogdandrutvik7@gmail.com>
2021-10-03 16:35:05 +0530

    Added styles to buttons, text and the input form

--- loan-emi-calc/src/components/InputForm.jsx
+++ loan-emi-calc/src/components/InputForm.jsx
@@ -23,7 +23,7 @@ function InputForm()
         let checkNum = event.target.value.split(".")
         if(checkNum.length === 1)
         {
-            setLoanAmt(Math.abs(event.target.value))
+            setLoanAmt(Math.abs(event.target.value) || "")
         }
        
     }
@@ -33,13 +33,13 @@ function InputForm()
         let checkNum = event.target.value.split(".")
         if(checkNum.length === 1)
         {
-            setLoanTenure(Math.abs(event.target.value))
+            setLoanTenure(Math.abs(event.target.value) || "")
         }
     }
 
     const handleIntRate = (event) => {
         setIntRateFlag(false)
-        setIntRate(Math.abs(Number(event.target.value).toFixed(2)))
+        setIntRate(Math.abs(Number(event.target.value).toFixed(2)) || "" )
     }
 
     const calcEmi = (amt, tenure, rate) => {
@@ -98,7 +98,7 @@ function InputForm()
     }
     return(
         <>
-            <h4>Loan EMI Calculator</h4>
+            <h4 style={{color:'#293A64'}} className="mt-2">Loan EMI Calculator</h4>
             <div className={styles.container}>
                 <div className={styles.inputFields}>
                     <div> Loan Amount </div>
@@ -128,23 +128,60 @@ function InputForm()
 
                 <div className={styles.inputFields}>
                     <div> Loan Tenure </div>
-                    <input className="form-control" type="number" name="loanTenure" placeholder="Enter months" value={loanTenure} onChange={handleLoanTenure}/>
+                    {
+                         emi && totalPayable && totalInterest ?
+                         <input value={loanTenure} className="form-control" />
+                         :
+                         <input className="form-control" type="number" name="loanTenure" placeholder="Enter months" value={loanTenure} onChange={handleLoanTenure}/>
+
+                    }
                     <div> Months </div>
                 </div>
+                {
+                    loanTenureFlag&&
+                    <div className="mt-2">
+                        <div className="alert alert-danger d-flex align-items-center" role="alert">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+                            <div>
+                                Please enter the loan tenure.
+                            </div>
+                        </div>
+                    </div>
+                }
                 <br/>
 
                 <div className={styles.inputFields}>
                     <div> Interest Rate </div>
-                    <input className="form-control" type="number" name="intRate" step=".01" pattern="^\d*(\.\d{0,2})?$" placeholder="NN.NN" value={intRate} onChange={handleIntRate}/>
+                    {
+                        emi && totalPayable && totalInterest ?
+                        <input value={intRate} className="form-control" />
+                        :
+                        <input className="form-control" type="number" name="intRate" step=".01" pattern="^\d*(\.\d{0,2})?$" placeholder="NN.NN" value={intRate} onChange={handleIntRate}/>
+                    }
                     <div> % </div>
                 </div>
+                {
+                    intRateFlag&&
+                    <div className="mt-2">
+                        <div className="alert alert-danger d-flex align-items-center" role="alert">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+                            <div>
+                                Please enter the interest rate.
+                            </div>
+                        </div>
+                    </div>
+                }
                 <br/>
                 
                 {
                     emi && totalPayable && totalInterest ?
-                    <button className onClick={handleReset}>Reset</button>
+                    <button style={{background:'#293A64', color:"white"}} className="btn " onClick={handleReset}>Reset</button>
                     :
-                    <button onClick={handleCalculate}>Calculate</button>
+                    <button style={{background:'#293A64', color:"white"}} className="btn" onClick={handleCalculate}>Calculate</button>
 
                 }
             </div>
--- loan-emi-calc/src/components/OutputForm.jsx
+++ loan-emi-calc/src/components/OutputForm.jsx
@@ -6,7 +6,7 @@ function OutputForm(props){
     console.log(props)
     return(
         <div>
-            <h4>Loan EMI Calculator</h4>
+            <h4 style={{color:'#293A64'}}>Loan EMI Calculator</h4>
             <div className={styles.OutputFormContainer}>
                 <div> 
                     <p>Loan EMI</p>
